cb9874f802cf9d43e44be7198caaefc4560812c7
Sahilrawat666 <sr4328914@gmail.com>
2026-06-21 12:34:54 +0530

    resume updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ function Hero() {
               Contact Me
             </a>
             <a
-              href="/Resume-Sahil-Rawat.pdf"
+              href="/Sahil_Rawat_Resume.pdf"
               download
               target="_blank"
               className="px-5 py-2.5 rounded-xl border text-gray-700 bg-white shadow hover:bg-gray-200 hover:-translate-y-1 transition-all duration-300"
