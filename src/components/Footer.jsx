090039962ba24f69180faad2c38cb7d9d40f693b
Sahilrawat666 <sr4328914@gmail.com>
2026-09-05 23:08:22 +0530

    Clean up unused portfolio files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
 
         <p className="font-mono text-sm leading-5 text-[#8892b0]">
           Designed &amp; built by
-          <span className="text-[#ccd6f6]">Sahil Rawat</span>
+          <span className="text-[#ccd6f6]"> Sahil Rawat</span>
         </p>
 
         <p className="font-mono text-xs text-[#495670]">
